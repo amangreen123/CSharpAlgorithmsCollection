378497ca9db0975a8bd1d803c46ed990df4f90f7
amangreen123 <Aaron.green1@marist.edu>
2024-09-23 01:26:16 -0400

    adding code

---
+++ Hash-Searching/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Hash_Searching
+{
+    internal class Program
+    {
+        static void HashandSearch()
+        {
+
+            string filepath = @"C:\Users\aaron\Desktop\magicitems.txt";
+            List<string> magicItems = new List<string>();
+            int linecount = 0;
+            Search Search = new Search();
+
+            try
+            {
+                //Passes file path and name to streamReader constructor 
+                StreamReader sr = new StreamReader(filepath);
+
+                using (sr)
+                {
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        linecount++;
+                        //ignores white space and Caps
+                        string lowerCase = line.ToLower();
+                        string cleanLine = string.Concat(lowerCase.Where(c => char.IsLetterOrDigit(c)));
+
+                        //adds lines into list 
+                        if (!string.IsNullOrEmpty(cleanLine)) // Only add non-empty lines
+                        {
+                            magicItems.Add(cleanLine);
+                            List<string> randomItems = GetRandomItems(magicItems, 42);
+
+                            foreach (var item in randomItems)
+                            {
+                               Search.LinearSearch(magicItems, item);
+
+                            }
+                        }
+
+                    }
+
+                }
+                sr.Close();
+                Console.ReadLine();
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Exception:" + e.Message);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            HashandSearch();
+        }
+
+        public static List<string> GetRandomItems(List<string> magicItems, int numberofElements)
+        {
+
+            var random = new Random();
+            var randomItems = new List<string>(magicItems.Count);  // Allocate memory for significant speedup.
+
+            for (int i = 0; i < numberofElements; i++)
+            {
+
+                var randomIndex = random.Next(0, magicItems.Count);
+                var randomItem = magicItems[randomIndex];
+                randomItems.Add(randomItem);
+            }
+
+            return randomItems;
+        }
+    }
+
+
+}
---
+++ Hash-Searching/Search.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_Searching
+{
+    internal class Search
+    {
+
+        public static int LinearSearch(List<string> magicItems, string target)
+        {
+            Console.WriteLine($"Starting Linear Search for '{target}'");
+
+            int comparisonCount = 0;
+
+            for (int i = 0; i < magicItems.Count; i++)
+            {
+
+                if (magicItems[i] == target)
+                {
+                    comparisonCount++;
+                    Console.WriteLine($"Found '{target}' after {comparisonCount} comparison{(comparisonCount > 1 ? "s" : "")} at index {i}");
+                    return i;
+                }
+
+            }
+            return -1;
+        }
+
+        public static int BinarySearch(List<string> magicItems, string target)
+        {
+            int left = 0;
+            int right = magicItems.Count - 1;
+            int mid;
+
+            int comparisonCount = 0;
+
+            while (left < right)
+            {
+                mid = left + (left + right) / 2;
+
+                int comparisonResult = string.Compare(target, magicItems[mid]);
+
+                if (comparisonResult > 0)
+                {
+                    left = mid + 1;
+                    comparisonCount++;
+                }
+                else if (comparisonResult < 0)
+                {
+                    right = mid - 1;
+
+                }
+                else
+                {
+                    Console.WriteLine($"Found '{target}' after {comparisonCount} comparisons at index {mid}");
+                    return mid;
+
+                }
+                Console.WriteLine($"Comparison {comparisonCount}: Checked index {mid} - '{magicItems[mid]}'");
+            }
+
+            Console.WriteLine($"'{target}' not found. Performed {comparisonCount} comparisons.");
+            return -1;
+        }
+    }
+}
